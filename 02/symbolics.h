25c20f9d0932cd7b62f71786e2743c665ed3d9f1
Sergei <sergeiyakima1@gmail.com>
2024-08-15 19:00:26 +0300

    renamed file, and changed value type

--- 02/symbolics.h
+++ 02/symbolics.h
@@ -1,11 +1,11 @@
-#ifndef _CALC_H
-#define _CALC_H
+#ifndef _SYMBOLICS_H
+#define _SYMBOLICS_H
 
 #define MAX_SYM_NUMBER 20 /* maximum number of symbols */
 
 extern struct symtab {
 	char *name;
-	int value;
+	float value;
 } g_symbals[MAX_SYM_NUMBER];
 
 struct symtab *symlook(char *sym);
